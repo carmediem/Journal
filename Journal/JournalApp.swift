ee77f685321b103cb4e169dd5173dd32c22be008
Carmen Chiu <carmenchiu@Carmens-iMac.local>
2022-07-06 09:46:42 -0700

    first commit

---
+++ Journal/JournalApp.swift
@@ -0,0 +1,17 @@
+//
+//  JournalApp.swift
+//  Journal
+//
+//  Created by Carmen Chiu on 7/6/22.
+//
+
+import SwiftUI
+
+@main
+struct JournalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
